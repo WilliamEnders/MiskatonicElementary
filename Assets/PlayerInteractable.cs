970c4a1b804db20bf8e5fa9a111faca112edbbe9
Kristian Bauer <kristianbauer@twitter.com>
2016-01-30 15:36:24 -0500

    Press E to interact with objects.

---
+++ Assets/PlayerInteractable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteractable : MonoBehaviour {
+
+	private PlayerInteractBox interactBox;
+
+	// Use this for initialization
+	void Start () {
+		interactBox = GameObject.FindGameObjectWithTag ("PlayerInteractBox").GetComponent<PlayerInteractBox>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && interactBox.interactObject != null) {
+			print ("player interact with: " + interactBox.interactObject.tag);
+			if (interactBox.interactObject.tag == "Bone") {
+				// do something w/ bone
+			} else if (interactBox.interactObject.tag == "Tree") {
+				// do something w/ tree
+			}
+		}
+	}
+}
