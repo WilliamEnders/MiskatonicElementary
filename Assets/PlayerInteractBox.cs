f408487c3a5742ec5afb8bb9b6c5cb1b28d801b4
Kristian Bauer <kristianbauer@twitter.com>
2016-01-30 15:12:55 -0500

    Adding interact box to player object so they can interact with objects.

---
+++ Assets/PlayerInteractBox.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteractBox : MonoBehaviour {
+
+	private Rigidbody rb;
+	public GameObject interactObject;
+
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		rb = player.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float input = Input.GetAxisRaw ("Horizontal");
+		if (input != 0) {
+			float xPos = (float)(input > 0 ? 0.8 : -0.8);
+			Vector3 position = rb.transform.position;
+			position.x += xPos;
+			transform.position = position;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		print ("Can interact with object: " + other.gameObject.tag);
+		interactObject = other.gameObject;
+	}
+
+	void OnTriggerExit(Collider other) {
+		print ("Can no longer interact with object: " + other.gameObject.tag);
+		interactObject = null;
+	}
+}
