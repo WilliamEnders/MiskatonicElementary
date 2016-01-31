490554f817628808414c7427a18d43c751425e7c
William Enders <endew231@newschool.edu>
2016-01-31 00:34:57 -0500

    working elemental star

---
+++ Assets/elementImageArray.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class elementImageArray : MonoBehaviour {
+
+	public Sprite[] tokens;
+	private Image img;
+	public int num;
+	private GameObject circle;
+	private elementArray script;
+
+	// Use this for initialization
+	void Start () {
+		circle = GameObject.Find ("Main Circle");
+		script = circle.GetComponent<elementArray> ();
+		img = GetComponent<Image> ();
+//		gameObject.SetActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		img.overrideSprite = tokens [script.elements[num]];
+	}
+}
