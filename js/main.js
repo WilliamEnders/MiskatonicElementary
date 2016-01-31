ecdf7794a4b7c4aef4dd33e75c71c1206041b7a0
Mark <mark@remarkablemark.org>
2016-01-31 18:15:08 +0000

    Add Phaser boilerplate

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Phaser = window.Phaser;
+var game = new Phaser.Game(800, 600, Phaser.AUTO);
+
+var GameState = {
+    preload: function() {},
+    create: function() {},
+    update: function() {}
+};
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
