df53d2cda363525c2bf3f4b7f60b2a83e3c2b49e
Kristian Bauer <kristianbauer@twitter.com>
2016-01-30 21:31:10 -0500

    Leaf vortex!

---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollow : MonoBehaviour {
+
+	private Rigidbody rb;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		rb = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = rb.transform.position + offset;
+	}
+}
